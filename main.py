746719ddf08339d9371af5e2122dda6ebed14f75
BRIGHTGAINSCOMPANY <162323582+BRIGHTGAINSCOMPANY@users.noreply.github.com>
2024-09-25 23:03:32 +0300

    start project

---
+++ main.py
@@ -0,0 +1,42 @@
+import customtkinter as ctk
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = ctk.CTk()
+app.title("BANKING APPLICATION")
+app.geometry("630x230")
+
+
+
+
+
+
+
+
+
+app.mainloop()
